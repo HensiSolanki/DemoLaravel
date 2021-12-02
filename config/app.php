eb72549f5d06edd20b2c78d6dbb8f8d2fcdbeb20
Hensi <hensisolanki@gmail.com>
2021-12-02 19:53:04 +0530

    Multi Auth with single Command

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Bmatovu\MultiAuth\MultiAuthServiceProvider::class,
         /*
          * Package Service Providers...
          */
